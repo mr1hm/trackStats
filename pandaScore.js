c180dcc9e7d2d8de5286e4b359506b10f6469d01
Kevin Ihm <kevinihm@gmail.com>
2019-09-24 15:20:13 -0700

    pandascore.js

---
+++ pandaScore.js
@@ -0,0 +1,28 @@
+class Pandascore {
+  constructor() {
+
+  }
+
+  getData() {
+    var ajaxConfig = {
+      url: 'https://api.pandascore.co/csgo/teams',
+      dataType: 'json',
+      method: 'get',
+      headers: {
+        Authorization: `Bearer bZHwdBze71eTDAcMfKzZrv56P5Bh4D7JCqyfuY7yM9KnyRPbDz4`,
+      },
+      data: {
+        game: 'csgo',
+        search: 'teams',
+      },
+      success: (response) => {
+        console.log(response);
+      },
+      error: (response) => {
+        console.log(response);
+      }
+
+    }
+    $.ajax(ajaxConfig);
+  }
+}
